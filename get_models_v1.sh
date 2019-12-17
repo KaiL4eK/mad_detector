1f2bd78960d2996ffdc7ca3549c8b5a7502b6f8d
KaiL4eK <darklexi494@gmail.com>
2019-12-17 22:42:49 +0300

    Unified downloading models

--- get_models_v1.sh
+++ get_models_v1.sh
@@ -4,20 +4,22 @@ HOST="194.85.169.205:8000"
 
 mkdir -p models; cd models
 
-VERSION="RF_model_YOLO"
+# VERSION="RF_model_YOLO_MbN"
+# BASE_MODEL="MbN2_416x416_t1"
 
-BASE_MODEL="MbN2_416x416_t1"
+VERSION="RF_model_YOLO_Tiny"
+BASE_MODEL="Tiny3_416x416_t1"
 
 FP16_MODEL="$BASE_MODEL"_FP16
 FP32_MODEL="$BASE_MODEL"_FP32
 
 BASE_URL="http://$HOST/test_data/$VERSION"
 
-wget -N "$BASE_URL/$BASE_MODEL.json"
+wget "$BASE_URL/$BASE_MODEL.json" -O "model.json"
 
-wget -N "$BASE_URL/$FP16_MODEL.bin"
-wget -N "$BASE_URL/$FP16_MODEL.mapping"
-wget -N "$BASE_URL/$FP16_MODEL.xml"
+wget "$BASE_URL/$FP16_MODEL.bin" -O "model.bin"
+wget "$BASE_URL/$FP16_MODEL.mapping" -O "model.mapping"
+wget "$BASE_URL/$FP16_MODEL.xml" -O "model.xml"
 
 # wget -N "$BASE_URL/$FP32_MODEL.bin"
 # wget -N "$BASE_URL/$FP32_MODEL.mapping"
